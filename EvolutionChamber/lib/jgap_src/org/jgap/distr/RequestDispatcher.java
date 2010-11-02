9ef247d8e3687d1be83100fd96297a0a7887cd35
Ximin Luo <infinity0@gmx.com>
2010-11-02 15:07:05 +0000

    rm binary jars from source tree

--- EvolutionChamber/lib/jgap_src/org/jgap/distr/RequestDispatcher.java
+++
@@ -1,8 +0,0 @@
-package org.jgap.distr;
-
-import java.io.*;
-
-public abstract class RequestDispatcher {
-  public abstract void dispatch(IWorker a_worker, WorkerCommand a_command)
-      throws IOException;
-}
