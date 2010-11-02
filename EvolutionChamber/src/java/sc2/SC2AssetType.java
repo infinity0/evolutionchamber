b5b034d3315e7889f38a0becf9442644d5a2ddc8
Ximin Luo <infinity0@gmx.com>
2010-11-02 19:12:54 +0000

    add reqs field for assets, and morph action stub files

--- EvolutionChamber/src/java/sc2/SC2AssetType.java
+++ EvolutionChamber/src/java/sc2/SC2AssetType.java
@@ -1,6 +1,7 @@
 package sc2;
 
 import java.util.EnumSet;
+import java.util.Set;
 import static sc2.SC2State.Race;
 
 /**
@@ -24,6 +25,12 @@ public class SC2AssetType {
 	final public SC2AssetType source;
 	/** morphs from */
 	final public SC2AssetType parent;
+	/**
+	** Requirements. eg. cyber core for stalker. Used by {@link SC2BuildAction}
+	** and {@link SC2UpgradeAction}. Protoss mothership has a separate build
+	** action and this field isn't necessary.
+	*/
+	final public Set<SC2AssetType> reqs;
 
 	final public int stat_hp;
 	final public int stat_sp;
@@ -47,55 +54,56 @@ public class SC2AssetType {
 	/** constructor for unit types */
 	public static SC2AssetType defineUnit(
 		Race race, int cost_m, int cost_v, int cost_t, int cost_f,
-		SC2AssetType source, SC2AssetType parent,
+		SC2AssetType source, SC2AssetType parent, Set<SC2AssetType> reqs,
 		int stat_hp, int stat_sp, int stat_ep,
 		double speed, int range, int sight, int size,
 		SC2Attack atk_g, SC2Attack atk_a, int armour, Modifier mod_0, Modifier ... mods
 	) {
 		return new SC2AssetType(race, cost_m, cost_v, cost_t, cost_f,
-		  source, parent, stat_hp, stat_sp, stat_ep,
+		  source, parent, reqs, stat_hp, stat_sp, stat_ep,
 		  speed, range, sight, size, atk_g, atk_a, armour, mod_0, mods);
 	}
 
 	/** constructor for structure types */
 	public static SC2AssetType defineStructure(
 		Race race, int cost_m, int cost_v, int cost_t,
-		SC2AssetType source,
+		SC2AssetType source, Set<SC2AssetType> reqs,
 		int stat_hp, int stat_sp, int stat_ep,
 		double speed, int range, int sight, int size,
 		SC2Attack atk_g, SC2Attack atk_a, int armour, Modifier ... mods
 	) {
 		return new SC2AssetType(race, cost_m, cost_v, cost_t, null,
-		  source, null, stat_hp, stat_sp, stat_ep,
+		  source, null, reqs, stat_hp, stat_sp, stat_ep,
 		  speed, range, sight, size, atk_g, atk_a, armour, Modifier.STRUCTURE, mods);
 	}
 
 	/** constructor for structure types that don't move, don't attack, and aren't add-ons */
 	public static SC2AssetType defineStructure(
 		Race race, int cost_m, int cost_v, int cost_t,
+		Set<SC2AssetType> reqs,
 		int stat_hp, int stat_sp, int stat_ep,
 		int range, int sight, int size,
 		int armour, Modifier ... mods
 	) {
 		return new SC2AssetType(race, cost_m, cost_v, cost_t, null,
-		  null, null, stat_hp, stat_sp, stat_ep,
+		  null, null, reqs, stat_hp, stat_sp, stat_ep,
 		  0.0, range, sight, size, null, null, armour, Modifier.STRUCTURE, mods);
 	}
 
 	/** constructor for tech types */
 	public static SC2AssetType defineTech(
 		Race race, int cost_m, int cost_v, int cost_t,
-		SC2AssetType source
+		SC2AssetType source, Set<SC2AssetType> reqs
 	) {
 		return new SC2AssetType(race, cost_m, cost_v, cost_t, null,
-		  source, null, 0, 0, 0,
+		  source, null, reqs, 0, 0, 0,
 		  0.0, 0, 0, 0, null, null, 0, Modifier.TECH);
 	}
 
 	/** custom constructor */
 	protected SC2AssetType(
 		Race race, int cost_m, int cost_v, int cost_t, Integer cost_f,
-		SC2AssetType source, SC2AssetType parent,
+		SC2AssetType source, SC2AssetType parent, Set<SC2AssetType> reqs,
 		int stat_hp, int stat_sp, int stat_ep,
 		double speed, int range, int sight, int size,
 		SC2Attack atk_g, SC2Attack atk_a, int armour, Modifier mod_0, Modifier ... mods
@@ -107,6 +115,7 @@ public class SC2AssetType {
 		this.cost_f = cost_f;
 		this.source = source;
 		this.parent = parent;
+		this.reqs = reqs;
 		this.stat_hp = stat_hp;
 		this.stat_sp = stat_sp;
 		this.stat_ep = stat_ep;
--- EvolutionChamber/src/java/sc2/action/SC2Action.java
+++ EvolutionChamber/src/java/sc2/action/SC2Action.java
@@ -1,15 +1,21 @@
 package sc2.action;
 
+import sc2.SC2State;
+
 /**
 ** Represents an ongoing action.
 */
 abstract public class SC2Action {
 
+	/** reference to the game state */
+	final protected SC2State game;
+
 	/** double because chrono boost can reduce it */
 	public double eta;
 
-	public SC2Action(int cost_t) {
-		eta = cost_t;
+	public SC2Action(SC2State game, int cost_t) {
+		this.game = game;
+		this.eta = cost_t;
 	}
 
 	public void advance() {
