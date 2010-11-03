dcf67688c28e08dd0580bff70cb8fcadb2301a95
Ximin Luo <infinity0@gmx.com>
2010-11-03 02:15:02 +0000

    BuildAddon is closer to a Morph than a Build

---
+++ EvolutionChamber/src/java/sc2/action/SC2MorphTerranAddon.java
@@ -0,0 +1,15 @@
+package sc2.action;
+
+import sc2.SC2State;
+
+/**
+** Represents an add-on build task. This can be thought of as a 1:2 morph from
+** structure to structure plus add-on. Like morphs, it can only be started when
+** the asset queue is empty, and whilst in progress, nothing else can be placed
+** onto the queue.
+*/
+public class SC2MorphTerranAddon /*extends SC2Morph*/ {
+
+	// TODO
+
+}
